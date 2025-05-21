a9d27555e5b4c0778f5bcccf45c498afe8c55835
gerard-lorente-lopez <lorentelopezgerard@gmail.com>
2025-05-21 16:41:05 +0200

    desplegament final arreglat

--- frontend_M12/src/app/app.routes.server.ts
+++ frontend_M12/src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'equip-edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'torneig-edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'torneig-declare-winner/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'ticket-queixa-edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
