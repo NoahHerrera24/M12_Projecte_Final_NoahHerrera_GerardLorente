0f3e14bd20abbbe5201800c4fcda161d2f8b88dd
Noah Herrera <noah-tadeo_herrerareyes@iescarlesvallbona.cat>
2025-05-23 17:04:49 +0200

    fix: add logging for file storage in createEquip method of ApiController

--- backend_M12/app/Http/Controllers/ApiController.php
+++ backend_M12/app/Http/Controllers/ApiController.php
@@ -303,6 +303,8 @@ class ApiController extends Controller
 
             $file->storeAs('/uploads/imatges/', $filename);
 
+            \Log::info("Archivo guardado en: " . $file);
+
             $equip->logo = $filename;
         }
 
